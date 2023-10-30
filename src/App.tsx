64b11208009b3f69ceb86b633861976535772f15
Wiktor <wiktor01@op.pl>
2023-10-30 16:51:11 +0100

    Add first change in project

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React na potrzeby projektu 
         </a>
       </header>
     </div>
